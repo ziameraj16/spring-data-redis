c81ce2ee312161e562ffdaf79ea6d1d9fd18f13b
mohammadz <ziameraj16@gmail.com>
2017-09-13 17:18:21 +0100

    Complete

---
+++ src/main/java/com/zia/Receiver.java
@@ -0,0 +1,27 @@
+package com.zia;
+
+import java.util.concurrent.CountDownLatch;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+
+/**
+ * Created by mohammadz on 13/09/2017.
+ */
+public class Receiver {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(Receiver.class);
+
+    private CountDownLatch latch;
+
+    @Autowired
+    public Receiver(CountDownLatch latch) {
+        this.latch = latch;
+    }
+
+    public void receiveMessage(String message) {
+        LOGGER.info("Received <" + message + ">");
+        latch.countDown();
+    }
+}
